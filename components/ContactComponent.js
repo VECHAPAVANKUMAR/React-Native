faac4918b89999bb7c4079f61f045f0927ced869
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-10-21 18:00:23 +0530

    implemented sending notifications and emails

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
+import * as MailComposer from 'expo-mail-composer';
 
 function Contact(props) {
+
+    const sendMail = () => {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
+
     return (
         <Card>
             <Card.Title>Contact Information</Card.Title>
@@ -15,6 +25,12 @@ function Contact(props) {
                 Fax: +852 8765 4321 {'\n'}
                 Email:confusion@food.net {'\n'} 
             </Text>
+            <Button
+                title="Send Email"
+                buttonStyle={{backgroundColor: "#512DA8"}}
+                icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                onPress={sendMail}
+            />
         </Card>
     );
 }
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker'
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
 
@@ -20,13 +21,64 @@ class Reservation extends Component {
         title: 'Reserve Table',
     };
 
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
+
     toggleModal() {
         this.setState({showModal: !this.state.showModal});
     }
 
     handleReservation() {
         console.log(JSON.stringify(this.state));
-        this.toggleModal();
+        let message = `
+                        Number of Guests: ${this.state.guests} \n
+                        Smoking? ${this.state.smoking} \n
+                        Date and Time: ${this.state.date}
+                    `
+        Alert.alert(
+            'Your Reservation Ok?',
+            message,
+            [
+                {
+                    text : 'Cancel', onPress: () => {
+                        console.log('Reservation Cancelled');
+                        this.resetForm();
+                    }, style: 'cancel'
+                },
+                {
+                    text : 'Ok', onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
+                }
+            ],
+            { cancelable : false }
+        );
+        // this.toggleModal();
     }
 
     resetForm() {
